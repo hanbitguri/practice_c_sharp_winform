7c036351869d1b0972af88182f941e2d811db3c0
LeeChangMyung <LeeChangMyung@AMD5900X>
2024-12-12 14:59:06 +0900

    초기 커밋.

---
+++ practice_2/Forms/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace practice_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(237, 264);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(465, 29);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "게임 규칙 : 플레이어는 번갈아 가면서 세 카드중 하나를 선택합니다. \r\n게임 마다 각 카드의 값은 랜덤으로 지정됩니다. 총 5턴을 진행합니다.\r" +
+    "\n\r\n";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(551, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "게임시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(237, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(151, 138);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "aScore";
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox2.Location = new System.Drawing.Point(394, 104);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(151, 138);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "bScore";
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox3.Location = new System.Drawing.Point(551, 104);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(151, 138);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "cScore";
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(237, 349);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(230, 184);
+            this.listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(472, 349);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(230, 184);
+            this.listBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(235, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(237, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 21);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(394, 49);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 21);
+            this.textBox3.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 567);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ practice_2/Forms/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace practice_2
+{
+   
+
+    public partial class Form1 : Form
+    {
+
+        class USER
+        {
+            public string userName = "";
+            public int aScore = 0;
+            public int bScore = 0;
+            public int cScore = 0;
+            public int count = 0;
+            public int sum = 0;
+
+            public static Random randomValue = new Random();
+            public USER()
+            {
+                this.aScore = randomValue.Next(1, 51);
+                this.bScore = randomValue.Next(1, 51);
+                this.cScore = randomValue.Next(1, 51);
+            }
+
+        }
+
+        USER user1 = new USER();
+        USER user2 = new USER();
+        
+
+
+        private USER currentUSer = null;
+        private bool gameStart = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void panel_Click(object sender, EventArgs e)
+        {
+            if (!gameStart)
+            {
+                return;
+            }
+            PictureBox choosedScore = (PictureBox)sender;
+            string typeOfScore = choosedScore.Tag.ToString();
+            FieldInfo score = typeof(USER).GetField(typeOfScore);
+
+            if (currentUSer.Equals(user1))
+            {
+                listBox1.Items.Add((String.Format("{0}(이/가) {1}점을 획득하였습니다.", currentUSer.userName, score.GetValue(user2))));
+                currentUSer.sum += (int)score.GetValue(user2);
+                currentUSer = user2;
+            }
+            else
+            {
+                listBox2.Items.Add((String.Format("{0}(이/가) {1}점을 획득하였습니다.", currentUSer.userName, score.GetValue(user1))));
+                currentUSer.sum += (int)score.GetValue(user1);
+                currentUSer = user1;
+
+            }
+            currentUSer.count++;
+
+            if(user1.count==5 && user2.count == 5)
+            {
+                string result = string.Format("게임이 끝났습니다. {0}는 {1}점 , {2}는 {3}점 으로 {4}가 이겼습니다.",
+                    user1.userName,user1.sum,user2.userName,user2.sum , user1.sum>user2.sum?user1.userName : user2.userName
+                    );
+                MessageBox.Show(result);
+            }
+
+            label1.Text = $"현재 차례 : {currentUSer.userName}";
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            panel_Click(sender, e);
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            panel_Click(sender, e);
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            panel_Click(sender, e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text)|| string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("플레이어 이름을 입력해주세요!");
+                return;
+            }
+            user1.userName = textBox2.Text;
+            user2.userName = textBox3.Text;
+
+            Random randomValue = new Random();
+            int initTurn = randomValue.Next(0, 2);
+            switch (initTurn)
+            {
+                case 0:
+                    currentUSer = user1;
+                    break;
+                case 1:
+                    currentUSer = user2;
+                    break;
+            }
+
+            TextBox[] textBoxes = { textBox2, textBox3 };
+            foreach (TextBox textBox in textBoxes)
+            {
+                textBox.Hide();
+            }
+
+            button1.Hide();
+            label1.Text = String.Format("현재 차례 : {0}", currentUSer.userName);
+            label1.Show();
+
+
+        }
+    }
+}
+
+
